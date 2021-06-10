56cd0b85ec2734823be41b18be540162bba3354d
Mak <85156926+cmboni@users.noreply.github.com>
2021-06-10 11:17:00 +0200

    email

--- components/login.js
+++ components/login.js
@@ -6,12 +6,9 @@ const uiConfig = {
   // Popup signin flow rather than redirect flow.
   signInFlow: "popup",
   // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: "/signedIn",
+  signInSuccessUrl: "/",
   // We will display Google and Facebook as auth providers.
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID
-  ]
+  signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID]
 };
 
 function SignInScreen() {
